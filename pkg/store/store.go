2fc660cca7834181f8320b8a190dc67e53d2c665
nyahahanoha <m21028@g.metro-cit.ac.jp>
2025-09-22 08:55:50 +0900

    feat: Implement prototype store

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,17 +1,42 @@
 package store
 
 import (
+	"fmt"
+	"log/slog"
+
 	bookscommon "github.com/BookManagementSystem/pkg/books/common"
 	storeconfig "github.com/BookManagementSystem/pkg/store/config"
+	"github.com/BookManagementSystem/pkg/store/db"
 )
 
 type BookStore struct {
+	db db.DBStore
 }
 
-func NewBooksStore(config storeconfig.Config) (BookStore, error) {
-	return BookStore{}, nil
+func NewBooksStore(lg *slog.Logger, config storeconfig.Config) (*BookStore, error) {
+	db, err := db.NewDBStore(lg, config.DB)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect db: %w", err)
+	}
+	if err := db.Init(); err != nil {
+		return nil, fmt.Errorf("failed to initialize db: %w", err)
+	}
+	return &BookStore{
+		db: db,
+	}, nil
 }
 
-func (s *BookStore) Put(book bookscommon.Info) error            { return nil }
-func (s *BookStore) Get(isbn string) (*bookscommon.Info, error) { return nil, nil }
-func (s *BookStore) Del(isbn string) error                      { return nil }
+func (s *BookStore) Put(book bookscommon.Info) error {
+	if err := s.db.Put(book); err != nil {
+		return fmt.Errorf("failed to put info in db: %w", err)
+	}
+	return nil
+}
+func (s *BookStore) Get(isbn string) (*bookscommon.Info, error) {
+	info, err := s.db.Get(isbn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get info in db: %w", err)
+	}
+	return info, nil
+}
+func (s *BookStore) Del(isbn string) error { return nil }
